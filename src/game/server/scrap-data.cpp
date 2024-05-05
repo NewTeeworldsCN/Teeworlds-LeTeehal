3e5807a5be39d24ac49ce1a50a84cd2947da1f62
ST-Chara <chara_kill_sans@163.com>
2024-05-05 13:26:18 +0800

    fix

--- src/game/server/scrap-data.cpp
+++ src/game/server/scrap-data.cpp
@@ -93,9 +93,7 @@ void CScrapInfo::ConMetalsheet(int ClientID, int ScrapID, void *pUser)
     CGameContext *pThis = (CGameContext *)pUser;
     CPlayer *pP = pThis->m_apPlayers[ClientID];
     
-    int Armor = 5;
-    if(pP->m_vScraps[ScrapID])
-        int Armor = pP->m_vScraps[ScrapID]->m_Weight / 2;
+    int Armor = rand()%7+1;
     pP->GetCharacter()->IncreaseArmor(Armor);
     pThis->SendChatTarget(ClientID, _("你装备了金属板.. 防御+{int:health}"), "health", &Armor);
 
