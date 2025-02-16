43e07e5d50f3a5409b9755f6bdcdc0100aa48d07
ST-Chara <chara_kill_sans@163.com>
2025-02-16 12:07:17 +0800

    Fix CMake

--- src/game/version.h
+++ src/game/version.h
@@ -2,7 +2,7 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#include "generated/nethash.cpp"
+
 #define GAME_VERSION "0.6.4 Lethal"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #define GAME_RELEASE_VERSION "0.6.4"
@@ -13,4 +13,5 @@
 #define MOD_CREDITS "Flower, ST-Chara"
 #define MOD_THANKS "Kurosio, Necropotame, GutZuFusss, Bamcane"
 #define MOD_SOURCES "https://github.com/NewTeeworldsCN/Teeworlds-LeTeehal"
+
 #endif
